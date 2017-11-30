8292a4e6e47535ab621742165b362ded0ee87798
MihaiChirculete <mihai.chirculete98@gmail.com>
2017-11-30 12:48:05 +0200

    Added GenericVariable data type. (used for local shell variables)

--- data_types/data_types.h
+++ data_types/data_types.h
@@ -3,9 +3,47 @@
 #define _SHELL_DATA_TYPES
 #endif
 
+#ifndef _GLIBCXX_STRING
+#include <string>
+#endif
+
+#ifndef _GLIBCXX_IOSTREAM
+#include <iostream>
+#endif
+
 using namespace std;
 
-struct ShellData{
-	string current_dir;	// the path of the directory we are inside
-	string prompt;	// the shell's prompt
+// used for local shell variables
+class GenericVariable{
+	private:
+		string name;
+		string value;
+
+	public:
+		GenericVariable(string name, string value)
+		{
+
+			this->name = name;
+			this->value = value;
+		}
+
+		string getName()
+		{
+			return this->name;
+		}
+
+		string getValue()
+		{
+			return this->value;
+		}
+
+		void setName(string name) { this->name = name; }
+		void setValue(string value) { this->value = value; }
+};
+
+class ShellData{
+	public:
+		string current_dir;	// the path of the directory we are inside
+		string prompt;	// the shell's prompt
+		string PATH;	// the PATH variable of the shell (where executables will be searched)
 };
