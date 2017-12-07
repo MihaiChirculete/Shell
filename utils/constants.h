62bbf9f6605c2ecb373f81c46c45adbe379ceb39
MihaiChirculete <mihai.chirculete98@gmail.com>
2017-12-07 17:53:18 +0200

    Added more constants

--- utils/constants.h
+++ utils/constants.h
@@ -16,5 +16,8 @@
 #define STATUS_WAITING 11
 #define STATUS_FINISHED 12
 
-/* REQUEST constants (20-?) */
+/* REQUEST constants (20-30) */
 #define REQUEST_TERMINATE_SHELL 20
+
+/* ERROR constants (30-?) */
+#define ERROR_FILE_NOT_FOUND 30
